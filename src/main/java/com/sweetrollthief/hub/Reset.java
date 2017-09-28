255b6f81a7981533f53bfd3662b6f3644327fa10
sweetrollthief <isa220400@gmail.com>
2017-09-28 21:06:49 +0300

    Initial commit

---
+++ src/main/java/com/sweetrollthief/hub/Reset.java
@@ -0,0 +1,11 @@
+package com.sweetrollthief.hub;
+
+/**
+* Main class, program entry point
+*
+**/
+public class Reset {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
