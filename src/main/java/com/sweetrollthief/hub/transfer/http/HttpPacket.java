668417814605482856b1145fb2e4d5c91be31d0f
sweetrollthief <isa220400@gmail.com>
2017-10-11 21:42:56 +0300

    little part of parsing

--- src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacket.java
+++ src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacket.java
@@ -1,9 +1,44 @@
 package com.sweetrollthief.hub.transfer.http;
 
+import java.util.List;
+import java.util.LinkedList;
+
 import com.sweetrollthief.hub.transfer.IPacket;
 
 public class HttpPacket implements IPacket {
+    enum SEGMENT_TYPE {
+        METHOD,
+        CODE,
+        CODE_DESCRIPTION,
+        PROTOCOL,
+        URI,
+        HEADER_KEY,
+        HEADER_VALUE,
+        BODY_SEGMENT_LENGTH,
+        BODY_SEGMENT_CONTENT
+    }
+    class Element {
+        private int position;
+        private int length;
+        private SEGMENT_TYPE type;
+
+        public Element(int position, int length, SEGMENT_TYPE type) {
+            this.position = position;
+            this.length = length;
+            this.type = type;
+        }
+        public int length() {
+            return length;
+        }
+        public int position() {
+            return position;
+        }
+    }
+
+    private int parsingPosition;
+
     private byte[] rawData;
+    private List<Element> elementList = new LinkedList<>();
 
     @Override
     public void setRawData(byte[] data) {
@@ -12,5 +47,37 @@ public class HttpPacket implements IPacket {
     @Override
     public byte[] getRawData() {
         return rawData;
+    }
+    void appendElement(int position, int length, SEGMENT_TYPE type) {
+        elementList.add(new Element(position, length, type));
+    }
+    List<Element> getElementList() {
+        return elementList;
+    }
+    /**
+    * @return current logical parsing part of message
+    * 0 - before first line
+    * 1 - first line
+    * 2 - header
+    * 3 - content
+    * 4 - after content
+    **/
+    int getParsingPosition() {
+        return parsingPosition;
+    }
+    void setParsingPosition(int position) {
+        this.parsingPosition = position;
+    }
+    /**
+    * @return position of last byte of last pasrsed element
+    *
+    */
+    int getCurrentPosition() {
+        if (elementList.size() == 0) {
+            return 0;
+        } else {
+            Element lastElement = elementList.get(elementList.size() - 1);
+            return lastElement.position() + lastElement.length();
+        }
     }
 }
--- src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacketParser.java
+++ src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacketParser.java
@@ -1,19 +1,61 @@
 package com.sweetrollthief.hub.transfer.http;
 
+import java.util.Arrays;
+import java.util.List;
+import java.io.ByteArrayInputStream;
+
 import com.sweetrollthief.hub.transfer.IPacketParser;
+import com.sweetrollthief.hub.transfer.http.HttpPacket.SEGMENT_TYPE;
+import com.sweetrollthief.hub.transfer.http.HttpPacket.Element;
 
 class HttpPacketParser implements IPacketParser<HttpPacket> {
     @Override
     public void parse(HttpPacket packet) {
-        byte[] packetData = packet.getRawData();
-        int size = packetData.length;
+        if (packet.getParsingPosition() > 3) return;
 
-        System.out.println();
+        byte[] data = packet.getRawData();
+        int position = packet.getCurrentPosition();
+        int size = data.length;
 
-        for (int i = 0; i < size; i++) {
-            System.out.print((char) packetData[i]);
+        System.out.println(position + " " + size + " " + packet.getParsingPosition());
+
+        while (position < size) {
+            switch (packet.getParsingPosition()) {
+                case 0:
+                case 1:
+                    position = parseFirstString(position, data, packet);
+                    break;
+                default:
+                    position++;
+            }
+        }
+    }
+
+    private int parseFirstString(int position, byte[] data, HttpPacket packet) {
+        int size = data.length;
+        List<Element> elementList = packet.getElementList();
+        boolean isRequest = false;
+        int lastPosition = position;
+
+        while (position < size) {
+            if (data[position] == 32) {
+                if (elementList.size() == 0) {
+                    System.out.println("FIRST: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
+                    packet.appendElement(lastPosition, position, SEGMENT_TYPE.METHOD); // if request
+                    lastPosition = position + 1;
+                } else {
+                    System.out.println("SECOND: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
+                    lastPosition = position + 1;
+                }
+            } else if (data[position] == 13) {
+                System.out.println("THIRD: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
+                packet.setParsingPosition(2);
+                return ++position;
+            }
+
+            position++;
         }
 
-        System.out.println();
+        return position;
     }
 }
