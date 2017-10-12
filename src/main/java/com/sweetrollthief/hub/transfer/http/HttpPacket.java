c7547318d95d2252f94d8d6b47206d7f6d37ed03
sweetrollthief <isa220400@gmail.com>
2017-10-12 21:55:08 +0300

    HTTP header parsing

--- src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacket.java
+++ src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacket.java
@@ -1,5 +1,9 @@
 package com.sweetrollthief.hub.transfer.http;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedList;
 
@@ -33,6 +37,9 @@ public class HttpPacket implements IPacket {
         public int position() {
             return position;
         }
+        public SEGMENT_TYPE type() {
+            return type;
+        }
     }
 
     private int parsingPosition;
@@ -47,6 +54,59 @@ public class HttpPacket implements IPacket {
     @Override
     public byte[] getRawData() {
         return rawData;
+    }
+    public String getMethod() {
+        return getEssential(SEGMENT_TYPE.METHOD);
+    }
+    public int getCode() throws NullPointerException {
+        String code = getEssential(SEGMENT_TYPE.CODE);
+
+        if (code == null) {
+            throw new NullPointerException();
+        }
+
+        return Integer.parseInt(code);
+    }
+    public String getProtocol() {
+        return getEssential(SEGMENT_TYPE.PROTOCOL);
+    }
+    public String getURI() {
+        return getEssential(SEGMENT_TYPE.URI);
+    }
+    public Map<String, String> getHeader() {
+        Map<String, String> header = new HashMap<>();
+
+        Iterator<Element> it = elementList.iterator();
+        Element tempKey, tempValue;
+
+        while (it.hasNext()) {
+            tempKey = it.next();
+
+            if (tempKey.type() == SEGMENT_TYPE.HEADER_KEY) {
+                tempValue = it.next();
+                header.put(
+                    new String(Arrays.copyOfRange(
+                        rawData, tempKey.position(), tempKey.position() + tempKey.length())),
+                    new String(Arrays.copyOfRange(
+                        rawData, tempValue.position(), tempValue.position() + tempValue.length())));
+            }
+        }
+
+        return header;
+    }
+    private String getEssential(SEGMENT_TYPE type) {
+        Iterator<Element> it = elementList.iterator();
+        Element temp;
+
+        while (it.hasNext()) {
+            temp = it.next();
+            if (temp.type() == type) {
+                return new String(Arrays.copyOfRange(
+                    rawData, temp.position(), temp.position() + temp.length()));
+            }
+        }
+
+        return null;
     }
     void appendElement(int position, int length, SEGMENT_TYPE type) {
         elementList.add(new Element(position, length, type));
--- src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacketParser.java
+++ src/main/java/com/sweetrollthief/hub/transfer/http/HttpPacketParser.java
@@ -1,6 +1,7 @@
 package com.sweetrollthief.hub.transfer.http;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 import java.io.ByteArrayInputStream;
 
@@ -25,32 +26,64 @@ class HttpPacketParser implements IPacketParser<HttpPacket> {
                 case 1:
                     position = parseFirstString(position, data, packet);
                     break;
+                case 2:
+                    position = parseHeaderSegment(position, data, packet);
+                    break;
                 default:
                     position++;
             }
         }
+
+        System.out.println("METHOD |" + packet.getMethod() + "|");
+        System.out.println("URI |" + packet.getURI() + "|");
+        System.out.println("PROTOCOL |" + packet.getProtocol() + "|");
+
+        Map<String, String> header = packet.getHeader();
+
+        for (Map.Entry<String, String> entry : header.entrySet()) {
+            System.out.println("HEADER_LINE |" + entry.getKey() + ":" + entry.getValue() + "|");
+        }
     }
 
+    private int parseHeaderSegment(int position, byte[] data, HttpPacket packet) {
+        int size = data.length;
+        int lastPosition = position;
+        boolean keyFound = false;
+
+        while (position < size) {
+            if (data[position] == 58 && !keyFound) {
+                packet.appendElement(lastPosition, position - lastPosition, SEGMENT_TYPE.HEADER_KEY);
+                lastPosition = position + 1;
+                keyFound = true;
+            } else if (data[position] == 13) {
+                packet.appendElement(lastPosition, position - lastPosition, SEGMENT_TYPE.HEADER_VALUE);
+                lastPosition = position + 2;
+                keyFound = false;
+            }
+
+            position++;
+        }
+
+        return ++position;
+    }
     private int parseFirstString(int position, byte[] data, HttpPacket packet) {
         int size = data.length;
-        List<Element> elementList = packet.getElementList();
         boolean isRequest = false;
         int lastPosition = position;
 
         while (position < size) {
             if (data[position] == 32) {
-                if (elementList.size() == 0) {
-                    System.out.println("FIRST: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
-                    packet.appendElement(lastPosition, position, SEGMENT_TYPE.METHOD); // if request
+                if (lastPosition == 0) {
+                    packet.appendElement(lastPosition, position - lastPosition, SEGMENT_TYPE.METHOD);
                     lastPosition = position + 1;
                 } else {
-                    System.out.println("SECOND: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
+                    packet.appendElement(lastPosition, position - lastPosition, SEGMENT_TYPE.URI);
                     lastPosition = position + 1;
                 }
             } else if (data[position] == 13) {
-                System.out.println("THIRD: |" + new String(Arrays.copyOfRange(data, lastPosition, position)) + "|");
+                packet.appendElement(lastPosition, position - lastPosition, SEGMENT_TYPE.PROTOCOL);
                 packet.setParsingPosition(2);
-                return ++position;
+                return position + 2;
             }
 
             position++;
